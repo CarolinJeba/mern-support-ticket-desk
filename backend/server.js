78d15e59c10f0d3431977113cbddecfd51279ec7
--replace-all <r.carolinjeba@outlook.com>
2023-03-05 18:40:59 +0530

    Connect to the mongoose database

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
+const { errorHandler } = require('./middleware/errorMiddleware');
+const connectDB = require('./config/db');
+
+// Connect to DB
+connectDB();
 
 const PORT = process.env.PORT || 8000;
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
   res.status(200).send('Hello! Welcome to Support Desk App');
@@ -12,5 +20,7 @@ app.get('/', (req, res) => {
 // Routes
 app.use('/api/users', require('./routes/userRoutes'));
 app.use('/api/users/login', require('./routes/userRoutes'));
+
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
